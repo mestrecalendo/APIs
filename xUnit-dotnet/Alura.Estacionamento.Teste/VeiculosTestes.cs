44f703c68f0d2b572ee2cbbe7458ae27d7c84152
Sofia Reis <57951365+mestrecalendo@users.noreply.github.com>
2023-09-11 16:57:58 -0400

    adicionado testes acelerar e frear veiculo

---
+++ xUnit-dotnet/Alura.Estacionamento.Teste/VeiculosTestes.cs
@@ -0,0 +1,33 @@
+using Alura.Estacionamento.Modelos;
+
+namespace Alura.Estacionamento.Teste
+{
+    public class VeiculosTestes
+    {
+        [Fact]
+        public void TestarVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Acelerar(10);
+
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestarVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Frear(10);
+
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
