cf70fae806a3edf118b897dadcb6bb7c174d41cf
Sofia Reis <57951365+mestrecalendo@users.noreply.github.com>
2023-09-11 17:31:08 -0400

    Adicionado teste - faturamento patio
    e tipo de teste Theory

---
+++ xUnit-dotnet/Alura.Estacionamento.Teste/PatioTestes.cs
@@ -0,0 +1,30 @@
+﻿using Alura.Estacionamento.Modelos;
+
+namespace Alura.Estacionamento.Teste
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = "Octavia Reis";
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "asd-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+        
+
+    }
+}
