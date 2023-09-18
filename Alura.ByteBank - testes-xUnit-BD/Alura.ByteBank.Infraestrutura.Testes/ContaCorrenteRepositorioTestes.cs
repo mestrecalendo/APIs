69d230597741459f702f38ea4515998981b24681
Sofia Reis <57951365+mestrecalendo@users.noreply.github.com>
2023-09-18 11:34:51 -0400

    Adicionado testes conta corrente

--- Alura.ByteBank - testes-xUnit-BD/Alura.ByteBank.Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
+++ Alura.ByteBank - testes-xUnit-BD/Alura.ByteBank.Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
@@ -64,5 +64,54 @@ namespace Alura.ByteBank.Infraestrutura.Testes
             //Assert
             Assert.NotNull(conta);
         }
+
+        [Fact]
+        public void TestaAtualizaSaldoDeterminadaConta()
+        {
+            //Arrange
+            var conta = _repositorio.ObterPorId(1);
+            double novoSaldo = 15;
+            conta.Saldo = novoSaldo;
+
+            //Act
+            var atualizarSaldo = _repositorio.Atualizar(1, conta);
+
+            //Assert
+            Assert.True(atualizarSaldo);
+        }
+
+        [Fact]
+        public void TestaInsereUmaNovaContaCorrenteNoBancoDeDados()
+        {
+            //Arrange           
+            var conta = new ContaCorrente()
+            {
+                Saldo = 10,
+                Identificador = Guid.NewGuid(),
+                Cliente = new Cliente()
+                {
+                    Nome = "Kent Nelson",
+                    CPF = "486.074.980-45",
+                    Identificador = Guid.NewGuid(),
+                    Profissao = "Bancário",
+                    Id = 1
+                },
+                Agencia = new Agencia()
+                {
+                    Nome = "Agencia Central Coast City",
+                    Identificador = Guid.NewGuid(),
+                    Id = 1,
+                    Endereco = "Rua das Flores,25",
+                    Numero = 147
+                }
+            };
+
+            //Act
+            var retorno = _repositorio.Adicionar(conta);
+
+            //Assert
+            Assert.True(retorno);
+
+        }
     }
 }
